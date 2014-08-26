86404a260baa4ec9d53e63474124fcba0c3dc874
HashBangStudio <hashbangstudio@users.noreply.github.com>
2014-08-26 12:28:20 +0000

    Move to switch csae construction
    Move to switch case construction and allow nomral and follow with no parameters
    
    Note remember have to add use Switch

--- 21-setCamera.pl
+++ 21-setCamera.pl
@@ -4,6 +4,7 @@ use strict;
 # We have to import the minecraft api module to do anything in the minecraft world
 use lib 'mcpi';
 use Minecraft;
+use Switch;
 use warnings;
 
 sub printAvailableCameraModes{
@@ -15,32 +16,53 @@ sub printAvailableCameraModes{
 # Any communication with the world must use this object
 my $mc = Minecraft::->create();
 
-my $minNumOfParams = 2;
+my $minNumOfParams = 1;
 my $numOfParamsGiven = @ARGV;
 
 if ($numOfParamsGiven >= $minNumOfParams){
+
     my $cameraMode = $ARGV[0];
-    print $cameraMode;
-    if ($cameraMode eq "follow"){
-        $mc->camera->setFollow($ARGV[1]);
-    }elsif ($cameraMode eq "normal"){
-        $mc->camera->setNormal($ARGV[1]);
-    }elsif ($cameraMode eq "fixed"){
-        if ($numOfParamsGiven == 4) {
-            #should verify arguments are integer coordinates
-            $mc->camera->setFixed();
-            $mc->camera->setPos($ARGV[1], 
-                                $ARGV[2], 
-                                $ARGV[3]);
-        }else{
+    print $cameraMode."\n";
+    switch ($cameraMode) {
+        case "follow" {
+            if ($numOfParamsGiven == 1) {
+                $mc->camera->setFollow();
+            }elsif ($numOfParamsGiven == 2) {
+                $mc->camera->setFollow($ARGV[1]);
+            }else{
+                print "Expected 1 or 2 parameters but got $numOfParamsGiven"
+            }
+        }
+        case "normal" {
+            if ($numOfParamsGiven == 1) {
+                $mc->camera->setNormal();
+            }elsif ($numOfParamsGiven == 2) {
+                $mc->camera->setNormal($ARGV[1]);
+            }else{
+                print "Expected 1 or 2 parameters but got $numOfParamsGiven"
+            }
+        }
+        case "fixed" {
+            if ($numOfParamsGiven == 4) {
+                #should verify arguments are integer coordinates
+                $mc->camera->setFixed();
+                $mc->camera->setPos($ARGV[1], 
+                                    $ARGV[2], 
+                                    $ARGV[3]);
+            }else{
                 print("insufficient parameters given. ");
                 print("Require 4 but got $numOfParamsGiven\n");
+                exit();
+            }
         }
-    }else{
-            print("Unknown camera mode parameter given ", $ARGV[1]);
+        else {
+            print("Unknown camera mode parameter given ", $ARGV[0]);
             printAvailableCameraModes();
+            exit();
+        }
     }
 }else{
     print("insufficient parameters given");
-    print("Require $minNumOfParams, got $numOfParamsGiven\n");
+    print("Require minimum of $minNumOfParams, got $numOfParamsGiven\n");
+    exit();
 }
