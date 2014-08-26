ff38189f775034448d8cd1154b58a1e5b163b483
HashBangStudio <hashbangstudio@users.noreply.github.com>
2014-08-26 12:29:37 +0000

    Fixes to example
    Remove srand call as repeatability not important and implicitly called
        at first rand call
    Clarify comment
    Get position after set to ensure has actually moved

--- 05-setPositionRandomly.pl
+++ 05-setPositionRandomly.pl
@@ -21,12 +21,8 @@ print $message, "\n";
 # send message to the minecraft chat
 $mc->postToChat($message);
 
-#Seed the random number generator (set it up)
-#This should not be called more than once per process (this script in this case)
-srand();
-
 # Set initial values for amount to shift horizontral coordinates by
-# rand() creates a number between 0 and number given (defaults to 1 if none given) 
+# rand() creates a number between 0 and 1 less than number given (defaults to 1 if none given) 
 my $xShift = int(rand(21))-10;
 my $zShift = int(rand(21))-10;
 
@@ -39,8 +35,10 @@ my $newYpos =  $mc->getHeight($newXpos, $newZpos);
 
 # Set the position of the player
 $mc->player->setTilePos($newXpos, $newYpos, $newZpos);
-#$mc->player->setPos($newXpos, $newYpos, $newZpos); # set actual position rather that Tile
-$message = "You have been moved to ($newXpos, $newZpos)";
+
+$playerPos = $mc->player->getTilePos();
+
+$message = "You have been moved to (".$playerPos->x.", ".$playerPos->z.")";
 
 print $message, "\n";
 # send message to the minecraft chat
